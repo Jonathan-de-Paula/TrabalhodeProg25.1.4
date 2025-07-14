ee68e6459ba3863627dbcfe2a05efad622b898f5
ChromastoneOMX <konohaskids@gmail.com>
2025-07-14 14:57:50 -0300

    Verifiquem o Log no servirdor de discord

--- trabalhoilum.cpp
+++ trabalhoilum.cpp
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include <math.h>
 
+double distance(int x1, int y1, int x2, int y2) {
+    return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}
+
     int main() {
         int m, n;
         double R;
